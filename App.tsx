4e509670a8106fd44494e62305537e8b90c56a64
Neha Sundar <nehasundar@Nehas-Air.broadband>
2023-01-23 16:54:09 +0000

    Started login page and simple design

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+    <View style={styles.inputView}>
+      <TextInput
+        style={styles.TextInput}
+        placeholder="Email"
+        placeholderTextColor="#f5f6f4"
+        onChangeText={(email) => setEmail(email)}
+        />
+        </View>
+    <View style={styles.inputView}>
+      <TextInput 
+      style={styles.TextInput}
+      placeholder="Password"
+      placeholderTextColor="#f5f6f4"
+      secureTextEntry={true}
+      onChangeText={(password) => setPassword(password)}
+      />
     </View>
+    </View>
+      
   );
 }
 
@@ -17,4 +36,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  inputView: {
+    backgroundColor: "#d08651",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center"
+  },
+
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding:10,
+    marginLeft: 20,
+  }
 });
