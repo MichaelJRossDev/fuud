6c9f35800f2fce03ab7d7141284f34f651f95a94
NuclearEngineer1 <ljk8@hotmail.co.uk>
2023-01-24 11:29:08 +0000

    add logIn/signOut user functions plus tests

--- __test__/users.test.ts
+++ __test__/users.test.ts
@@ -1,5 +1,7 @@
-import * as users from '../src/users';
-import {getAuth} from 'firebase-admin/auth'
+import * as users from "../src/users";
+import { getAuth } from "firebase-admin/auth";
+import { auth } from "../config/firebaseConfig";
+import { signOut } from "firebase/auth";
 
 const admin = require("firebase-admin");
 
@@ -7,22 +9,32 @@ const serviceAccount = require("../config/fuud-34a91-firebase-adminsdk-2xmkp-f8b
 
 const adminApp = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://fuud-34a91-default-rtdb.europe-west1.firebasedatabase.app"
+  databaseURL:
+    "https://fuud-34a91-default-rtdb.europe-west1.firebasedatabase.app",
 });
 
 const adminAuth = getAuth(adminApp);
 
-let currentId:string;
+let currentId: string;
 
-describe('Create User', () => {
-    test('should create user in database', async () => {
-        await users.createUser('testemail@hotmail.com', 'testPassword')
-        .then(() => adminAuth.getUserByEmail('testemail@hotmail.com'))
-        .then((user) => {
-            currentId = user.uid;
-            expect(user.email).toBe('testemail@hotmail.com')
-            return user;
-        })
-        .then((user) => adminAuth.deleteUser(user.uid))        
-    });
+describe("Create User", () => {
+  test("should create user in database", async () => {
+    await users
+      .createUser("testemail@hotmail.com", "testPassword")
+      .then(() => adminAuth.getUserByEmail("testemail@hotmail.com"))
+      .then((user) => {
+        currentId = user.uid;
+        expect(user.email).toBe("testemail@hotmail.com");
+        return user;
+      })
+      .then((user) => adminAuth.deleteUser(user.uid));
+  });
+});
+
+describe("Sing in user", () => {
+  test("should sign in user", async () => {
+    await users.signOutUser()
+    await users.logInUser("michaeljrossdev@gmail.com", "password");
+      expect(auth.currentUser!.email).toBe("michaeljrossdev@gmail.com");
+  });
 });
--- src/users.ts
+++ src/users.ts
@@ -1,14 +1,30 @@
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
-import { app, db, auth } from '../config/firebaseConfig';
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { app, db, auth } from "../config/firebaseConfig";
 
-export const signIn = (email:string, password:string) => {
-    signInWithEmailAndPassword(auth, email, password)
-        .then(() => console.log("Login success"))
-        .catch((err) => console.log("Login failure"));
-}
+export const signIn = async (email: string, password: string) => {
+  await signInWithEmailAndPassword(auth, email, password)
+    .then(() => console.log("Login success"))
+    .catch((err) => console.log("Login failure"));
+};
 
-export const createUser = (email:string, password:string) => {
-    return createUserWithEmailAndPassword(auth, email, password)
-        .then(() => console.log("User created"))
-        .catch((err) => console.log("Error during user creation"))
-}
+export const createUser = async(email: string, password: string) => {
+  return await createUserWithEmailAndPassword(auth, email, password)
+    .then(() => console.log("User created"))
+    .catch((err) => console.log("Error during user creation"));
+};
+
+export const logInUser = async (email: string, password: string) => {
+  await signInWithEmailAndPassword(auth, email, password)
+    .then(() => console.log("User signed in successfully"))
+    .catch((err) => console.log(err));
+};
+
+export const signOutUser = async () => {
+  await signOut(auth)
+    .then(() => console.log("User signed out successfully"))
+    .catch((err) => console.log(err));
+};
