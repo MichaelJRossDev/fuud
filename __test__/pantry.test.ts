417196ac4c28b3f31e7f40f210768e1006494121
MSalim16 <msalim16@DESKTOP-DAK50HL.localdomain>
2023-01-25 15:26:05 +0000

    add getAllItems function

---
+++ __test__/pantry.test.ts
@@ -0,0 +1,121 @@
+import * as pantry from "../src/pantry";
+import { get, ref, getDatabase, child } from "firebase/database";
+import { app, db, auth } from "../config/firebaseConfig";
+import { signIn, signOutUser } from "../src/users";
+
+beforeAll(async () => {
+  await signOutUser();
+  await signIn("michaeljrossdev@gmail.com", "password");
+});
+
+beforeEach(async () => {
+  await pantry.emptyPantry();
+});
+
+afterAll(async () => {
+  await pantry.emptyPantry();
+  await signOutUser();
+});
+
+describe("Add item", () => {
+  test("Should add items to the Pantry", async () => {
+    const item: pantry.PantryItem = {
+      name: "banana",
+      expiry: Number(new Date(2024, 1, 1)),
+      category: "Fruit",
+      quantity: 5,
+      unit: "units",
+      item_id: 200,
+    };
+    await pantry.addItem({ ...item });
+    const dbRef = ref(db);
+
+    await get(
+      child(dbRef, `${auth.currentUser!.uid}` + "/pantry/" + item.item_id)
+    ).then(snapshot => {
+      const dbItem = snapshot.val();
+
+      expect(dbItem).toEqual(item);
+    });
+  });
+
+  test("Should add item without item_id", async () => {
+    const item: pantry.PantryItem = {
+      name: "pineapple",
+      expiry: Number(new Date(2024, 1, 1)),
+      category: "Fruit",
+      quantity: 5,
+      unit: "units",
+    };
+    const dbRef = ref(db);
+
+    await pantry.addItem({ ...item });
+
+    await get(child(dbRef, `${auth.currentUser!.uid}` + "/pantry/"))
+      .then(snapshot => snapshot.val())
+      .then(data => Object.values(data)[0])
+      .then(object => {
+        expect(object).toMatchObject({
+          name: "pineapple",
+          expiry: Number(new Date(2024, 1, 1)),
+          category: "Fruit",
+          quantity: 5,
+          unit: "units",
+          item_id: expect.any(Number),
+        });
+      });
+  });
+});
+
+describe("emptyPantry", () => {
+  test("pantry is emptied", async () => {
+    for (let i = 0; i < 5; i++) {
+      pantry.addItem({
+        name: i.toString(),
+        expiry: Number(new Date(2024, 1, i)),
+        category: (i * 2).toString(),
+        quantity: i * 3,
+        unit: "Lightyears",
+        item_id: Number(Date.now()),
+      });
+    }
+    pantry.emptyPantry();
+    const dbRef = ref(db);
+    await get(child(dbRef, `${auth.currentUser!.uid}` + "/pantry/"))
+      .then(snapshot => snapshot.val())
+      .then(data => {
+        expect(data).toEqual(null);
+      });
+  });
+});
+
+describe("getPantry", () => {
+  test("gets all items from pantry", async () => {
+    for (let i = 0; i < 5; i++) {
+      await pantry.addItem({
+        name: i.toString(),
+        expiry: Number(new Date(2024, 1, i)),
+        category: (i * 2).toString(),
+        quantity: i * 3,
+        unit: "Lightyears",
+        item_id: Number(Date.now()),
+      });
+    }
+    // const list = await pantry.getPantry();
+    const pantryItems = await pantry.getPantry();
+    const values = Object.values(pantryItems);
+    expect(values).toHaveLength(5);
+    expect(values).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: expect.any(String),
+          expiry: expect.any(Number),
+          category: expect.any(String),
+          quantity: expect.any(Number),
+          unit: expect.any(String),
+          item_id: expect.any(Number),
+        }),
+      ])
+    );
+  });
+});
---
+++ src/pantry.ts
@@ -0,0 +1,47 @@
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { app, db, auth } from "../config/firebaseConfig";
+
+import { set, ref, get, child } from "firebase/database";
+
+export interface PantryItem {
+  name: string;
+  expiry: number;
+  category: string;
+  quantity: number;
+  unit: string;
+  item_id?: number;
+}
+
+export const addItem = async (item: PantryItem) => {
+  const item_id = item.item_id ? item.item_id : Number(Date.now());
+  await set(ref(db, `${auth.currentUser!.uid}` + "/pantry/" + item_id), {
+    name: item.name,
+    expiry: item.expiry,
+    category: item.category,
+    quantity: item.quantity,
+    unit: item.unit,
+    item_id: item_id,
+  });
+};
+
+export const emptyPantry = async () => {
+  await set(ref(db, `${auth.currentUser!.uid}/pantry`), null);
+};
+
+export const getPantry = async () => {
+  let pantryItems = {};
+  await get(child(ref(db), `${auth.currentUser!.uid}` + "/pantry/"))
+    .then(snapshot => snapshot.val())
+    .then(data => {
+      pantryItems = data;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  return pantryItems;
+};
