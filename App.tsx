21eaff0cc130ddafaeb0d3298c0c4e11bfe712d0
lisaCodes92 <lisamslloyd@gmail.com>
2023-01-30 13:50:56 +0000

    starts second phase in pantry

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import LogIn from "./components/Login"
 import Homepage from "./components/Homepage";
 
 export default function App() {
-  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [loggedIn, setLoggedIn] = useState<boolean>(true);
 
   if (loggedIn === false) {
     return <LogIn setLoggedIn={setLoggedIn} />;
