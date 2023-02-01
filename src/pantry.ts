f2efac4166ec2e576ebbe93bee2d18756256076e
NuclearEngineer1 <ljk8@hotmail.co.uk>
2023-02-01 11:30:57 +0000

    change getItemInfoByBarcode to use axios

--- src/pantry.ts
+++ src/pantry.ts
@@ -8,13 +8,8 @@ import { app, db, auth } from "../config/firebaseConfig";
 import { set, ref, get, child } from "firebase/database";
 
 import FuzzySearch from "fuzzy-search";
-import { Console } from "console";
 
-const off = require("openfoodfacts-nodejs");
-
-const client = new off();
-
-client.getProduct("30720601").then(console.log);
+import axios from "axios"
 
 export interface PantryItem {
   name: string;
@@ -101,10 +96,10 @@ export const searchPantry = async (
 
 export const getItemInfoByBarcode = async (barcode: string) => {
   let item: object = {}
-  const client = new off();
   let quantityJustNumber: string = ""
   let quantityJustUnit:string = ""
-  await client.getProduct(barcode).then((itemInfo: any) => {
+  await axios.get(`https://world.openfoodfacts.org/api/v2/product/${barcode}`).then((itemInfo: any) => {
+    console.log(itemInfo)
     if (itemInfo.status === "0") {
       console.log("item not found")
       return {
@@ -115,42 +110,42 @@ export const getItemInfoByBarcode = async (barcode: string) => {
       }
     } else {
       
-      if (itemInfo.product.quantity) {
-        if (itemInfo.product.quantity.includes(",")) {
-          const quantityStringArray:Array<string> = itemInfo.product.quantity.split(",")
+      if (itemInfo.data.product.quantity) {
+        if (itemInfo.data.product.quantity.includes(",")) {
+          const quantityStringArray:Array<string> = itemInfo.data.product.quantity.split(",")
           const quantityWithUnits:string = quantityStringArray[0]
           const quantityUnitArray:Array<string> = quantityWithUnits.split(" ")
           quantityJustNumber = quantityUnitArray[0]
           quantityJustUnit = quantityUnitArray[1]
         } else {
-          const quantityUnitArray:Array<string> = itemInfo.product.quantity.split(" ")
+          const quantityUnitArray:Array<string> = itemInfo.data.product.quantity.split(" ")
           quantityJustNumber = quantityUnitArray[0]
           quantityJustUnit = quantityUnitArray[1]
         }
       }
 
       let category: string = ""
-      console.log(itemInfo.product.category, "category name");
+      console.log(itemInfo.data.product.category, "category name");
 
 
-      if (itemInfo.product.categories_hierarchy) {
-        if (itemInfo.product.categories_hierarchy[0].includes(":")) {
-          let categoryStringArray: Array<string> = itemInfo.product.categories_hierarchy[0].split(":")
+      if (itemInfo.data.product.categories_hierarchy) {
+        if (itemInfo.data.product.categories_hierarchy[0].includes(":")) {
+          let categoryStringArray: Array<string> = itemInfo.data.product.categories_hierarchy[0].split(":")
           category = categoryStringArray[1]
         } else {
-          category = itemInfo.product.categories_hierarchy[0]
+          category = itemInfo.data.product.categories_hierarchy[0]
         }
       }
       
       item = {
-        name: itemInfo.product.product_name ? itemInfo.product.product_name : "",
+        name: itemInfo.data.product.product_name ? itemInfo.data.product.product_name : "",
         category: category,
         quantity: quantityJustNumber ? Number(quantityJustNumber) : 0,
         unit: quantityJustUnit
       }
     
     }
-  })
+  }).catch(console.log)
   return item;
 }
 
